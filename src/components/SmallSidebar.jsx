6054155c3f225390e58a84b3c06d6da2e4fc7c6f
molu <molu.developer@gmail.com>
2023-11-20 10:41:13 -0300

    Login page estructura y estilos

---
+++ src/components/SmallSidebar.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Wrapper from '../assets/wrappers/SmallSidebar';
+
+const SmallSidebar = () => {
+  return <Wrapper>Small Sidebar</Wrapper>;
+};
+
+export default SmallSidebar;
