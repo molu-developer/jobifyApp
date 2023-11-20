e2f5098fb53b5c165c0004e1aaea133189b58e9b
molu <molu.developer@gmail.com>
2023-11-20 11:21:04 -0300

    set up react context

--- src/components/SmallSidebar.jsx
+++ src/components/SmallSidebar.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import { useDashboardContext } from '../pages/DashBoardLayout';
 
 const SmallSidebar = () => {
+  const data = useDashboardContext();
+  console.log(data);
   return <Wrapper>Small Sidebar</Wrapper>;
 };
 
