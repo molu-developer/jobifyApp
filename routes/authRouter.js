d0f4b048bd828245c57b693c045eea4314be8aa0
molu <molu.developer@gmail.com>
2023-12-11 07:20:33 -0300

    set up cookie and jwt, finally login validation

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -1,8 +1,9 @@
 import { Router } from "express";
 const router = Router();
 import { login, register } from '../controllers/authController.js';
+import { validateLoginInput, validateRegisterInput } from '../middleware/validationMiddleware.js';
 
-router.post('/register', register);
-router.post('/login', login);
+router.post('/register', validateRegisterInput, register);
+router.post('/login', validateLoginInput, login);
 
 export default router;
