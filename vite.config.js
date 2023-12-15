8c757069d3b455280c2a6caffc89868cfd74f172
molu <molu.developer@gmail.com>
2023-12-15 08:17:39 -0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-<<<<<<< HEAD
-})
-=======
   server: {
     proxy: {
       '/api': {
@@ -17,4 +14,3 @@ export default defineConfig({
     },
   },
 });
->>>>>>> ba2b357166e3ca32369e66ec9fef34a757a0d239
